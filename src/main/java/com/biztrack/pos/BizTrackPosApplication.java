cba63f90bde6e353ffefabec0a4f35a55601a7b7
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-14 22:19:35 +0530

    chore: initialize Spring Boot backend for multi-tenant POS SaaS

---
+++ src/main/java/com/biztrack/pos/BizTrackPosApplication.java
@@ -0,0 +1,13 @@
+package com.biztrack.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizTrackPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizTrackPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biztrack/pos/BizTrackPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biztrack.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizTrackPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
