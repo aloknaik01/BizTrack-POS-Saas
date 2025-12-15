5c744d6e24ed1b63f08d5c4aaabb75d8d7990a86
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-15 23:18:18 +0530

    git commit -m "feat(jwt): add JWT constants for header and secret"

---
+++ src/main/java/com/biztrack/pos/configuration/JwtConstants.java
@@ -0,0 +1,6 @@
+package com.biztrack.pos.configuration;
+
+public class JwtConstants {
+public  static final String JWT_SECRET="asldjaiqury982749832ukjhwiuy4983y4u2y52u34g5jg234uy5t235w";
+    public  static final String JWT_HEADER = "Authorization";
+}
