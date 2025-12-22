b96d3c19bd17cd99217a1d290e4b06311ffab854
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-22 22:40:36 +0530

    feat(user): implement service logic for get user from jwt token

---
+++ src/main/java/com/biztrack/pos/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.biztrack.pos.service.impl;
+
+import com.biztrack.pos.configuration.JwtProvider;
+import com.biztrack.pos.exceptions.UserException;
+import com.biztrack.pos.models.User;
+import com.biztrack.pos.repository.UserRepository;
+import com.biztrack.pos.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+
+    @Override
+    public User getUserFromJwtToken(String token) throws UserException {
+        String email = jwtProvider.getEmailFromToken(token);
+        User user = userRepository.findByEmail(email);
+        if(user == null) {
+            throw new UserException("Invalid Token");
+        }
+        return user;
+    }
+
+    @Override
+    public User getCurrentUser() {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return List.of();
+    }
+}
